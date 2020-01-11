7fd7aef7d53da892db3b937bff71ba05b22ef513
Joey Lorich <joseph@lorich.me>
2020-01-10 23:15:22 -0700

    update comments

--- provision.sh
+++ provision.sh
@@ -7,6 +7,8 @@ USERNAME=`terraform output username`;
 PASSWORD=`terraform output password`;
 IOTHUB_HOSTNAME=`terraform output iothub_hostname`;
 IOTHUB_NAME=`terraform output iothub_name`;
+DPS_NAME=`terraform output dps_name`;
+RG_NAME=`terraform output resource_group_name`;
 
 # Allow SSH to VM
 ssh-keygen -R $FACTORYMONITOR_IP
@@ -15,3 +17,6 @@ ssh-keyscan $FACTORYMONITOR_IP >> ~/.ssh/known_hosts
 # Configure the device VM
 ansible-playbook -i $FACTORYMONITOR_IP, -e "ansible_user=$USERNAME ansible_ssh_pass=$PASSWORD ansible_become_pass=$PASSWORD iothub_hostname='$IOTHUB_HOSTNAME' iothub_name='$IOTHUB_NAME'" playbook.yml
 
+# Create an enrollment group for DPS
+#  -- note, this will NOT enabl3e edge devices.  That is not currently part of the CLI.  Edge must be turned on in the portal.
+az iot dps enrollment-group create -g $RG_NAME --dps-name $DPS_NAME --enrollment-id openhack_devices
