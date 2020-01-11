4e6f2210ccf9e72ac9336995b1d38d955ad00b90
Joey Lorich <joseph@lorich.me>
2020-01-10 22:38:32 -0700

    updates

--- provision.sh
+++ provision.sh
@@ -1,12 +1,17 @@
+# Pave down the infrastrcture
 terraform apply --auto-approve;
 
+# Get Infrastructure details
 FACTORYMONITOR_IP=`terraform output factorymonitor_ip`;
 USERNAME=`terraform output username`;
 PASSWORD=`terraform output password`;
 IOTHUB_HOSTNAME=`terraform output iothub_hostname`;
 IOTHUB_NAME=`terraform output iothub_name`;
 
+# Allow SSH to VM
 ssh-keygen -R $FACTORYMONITOR_IP
 ssh-keyscan $FACTORYMONITOR_IP >> ~/.ssh/known_hosts
 
+# Configure the device VM
 ansible-playbook -i $FACTORYMONITOR_IP, -e "ansible_user=$USERNAME ansible_ssh_pass=$PASSWORD ansible_become_pass=$PASSWORD iothub_hostname='$IOTHUB_HOSTNAME' iothub_name='$IOTHUB_NAME'" playbook.yml
+
